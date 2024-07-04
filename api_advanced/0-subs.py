d520536bb3d6a979644697b4ec41a1fdca5c1767
root <j.nformi1@alustudent.com>
2024-07-04 04:38:32 -0700

    Tasks

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -3,15 +3,16 @@
 
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """function that fetches number_of_subscribers"""
-    URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
-
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'myRedditApp/0.0.1'}
+    
     try:
-        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
-        return RESPONSE.json().get("data").get("subscribers")
-
-    except Exception:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
+    except Exception as e:
         return 0
